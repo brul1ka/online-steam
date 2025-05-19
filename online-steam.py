1f7a75ca4c8fee1f1dc8ca4f9c440e34cc484b2d
brul1ka <yr.bunik@gmail.com>
2025-05-19 20:54:50 +0300

    base logic

--- online-steam.py
+++ online-steam.py
@@ -1,1 +1,8 @@
-print('Hello, world!')
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get("https://steamcharts.com/app/367520") # Hollow Knight
+soup = BeautifulSoup(response.text, 'html.parser')
+player_count = soup.find('div', class_='app-stat').find('span').text
+
+print(player_count)
