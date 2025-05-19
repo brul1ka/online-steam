5c78cfbca1bd26ea17ebc3ea18424a138dd694d3
brul1ka <yr.bunik@gmail.com>
2025-05-19 21:06:36 +0300

    new .bat for convenient launch of the application

--- online-steam.py
+++ online-steam.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get("https://steamcharts.com/app/367520") # Hollow Knight
+response = requests.get("https://steamcharts.com/app/367520") # Hollow Knight - for a test
 soup = BeautifulSoup(response.text, 'html.parser')
 player_count = soup.find('div', class_='app-stat').find('span').text
 
