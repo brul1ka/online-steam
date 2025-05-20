24d1c66d5619a8b6bcba43774ca9c0bee19922d6
brul1ka <yr.bunik@gmail.com>
2025-05-20 12:09:26 +0300

    error handler and search for appid by game name, search name by id removed

--- online-steam.py
+++ online-steam.py
@@ -1,23 +1,32 @@
 import requests
 
-appid = 367520  # Hollow Knight - just for a test
-
-# getting the number of players at the moment
-url = f"https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid={appid}"
-response = requests.get(url)
-data = response.json()
-player_count = data['response']['player_count']
+error_player_count = 25188734
+game_name = 'Portal 2' # Portal 2 - for a tests
 
 # getting app list with appid and their names
 app_list_url = "https://api.steampowered.com/ISteamApps/GetAppList/v2/"
 app_list_response = requests.get(app_list_url)
 apps_data = app_list_response.json()
 
-# getting app name
-app_name = None
+# getting appid by app name
+appid = None
 for app in apps_data['applist']['apps']:
-    if app['appid'] == appid:
-        app_name = app['name']
+    if app['name'] == game_name:
+        appid = app['appid']
         break
+    else:
+        appid = 'no data'
 
-print(f"{app_name} current players: {player_count}")
+# getting the number of players at the moment
+try:
+    url = f"https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid={appid}"
+    response = requests.get(url)
+    data = response.json()
+    player_count = data['response']['player_count']
+    if player_count == error_player_count:
+        player_count = '!!an error occurred while searching for the game!!'
+except KeyError:
+    print('No such a game by this appid')
+    player_count = 'no data'
+
+print(f"{game_name} current players: {player_count}")
