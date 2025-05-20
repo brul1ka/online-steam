820fdb0090c1edf2c1983d8afa2aed276f42d214
brul1ka <yr.bunik@gmail.com>
2025-05-20 10:55:22 +0300

    getting info via SteamAPI, getting name via appid

--- online-steam.py
+++ online-steam.py
@@ -1,8 +1,23 @@
 import requests
-from bs4 import BeautifulSoup
 
-response = requests.get("https://steamcharts.com/app/367520") # Hollow Knight - for a test
-soup = BeautifulSoup(response.text, 'html.parser')
-player_count = soup.find('div', class_='app-stat').find('span').text
+appid = 367520  # Hollow Knight - just for a test
 
-print(player_count)
+# getting the number of players at the moment
+url = f"https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid={appid}"
+response = requests.get(url)
+data = response.json()
+player_count = data['response']['player_count']
+
+# getting app list with appid and their names
+app_list_url = "https://api.steampowered.com/ISteamApps/GetAppList/v2/"
+app_list_response = requests.get(app_list_url)
+apps_data = app_list_response.json()
+
+# getting app name
+app_name = None
+for app in apps_data['applist']['apps']:
+    if app['appid'] == appid:
+        app_name = app['name']
+        break
+
+print(f"{app_name} current players: {player_count}")
