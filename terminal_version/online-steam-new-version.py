c06405ed08126c92edf8ae850e76e3db2f517b04
brul1ka <yr.bunik@gmail.com>
2025-05-25 12:25:27 +0300

    Basic functions have been rewritten to Textual

--- terminal_version/online-steam-new-version.py
+++ terminal_version/online-steam-new-version.py
@@ -3,6 +3,8 @@ from textual.widgets import Static, Input
 from textual.containers import Container
 from textual import on
 import requests
+import asyncio
+
 
 class MyApp(App):
     CSS = """
@@ -11,10 +13,10 @@ class MyApp(App):
         }
         #loading {
             dock: bottom;
-            align: center middle;
+            align: center bottom;
             padding: 1;
         }
-    
+
     """
 
     def find_appid(self, apps, name):
@@ -35,14 +37,30 @@ class MyApp(App):
         yield Static('', id='output')
         yield Static("", id='loading')
 
-    def on_mount(self):
+    async def on_mount(self):
         loading_widget = self.query_one("#loading", Static)
         loading_widget.update("Loading list of games...")
-        self.apps = self.get_games_list()
+        self.apps = await asyncio.to_thread(self.get_games_list)
         loading_widget.update("Game list loaded successfully.")
 
     @on(Input.Submitted)
-    def on_game_input_submitted(self, event: Input.Submitted):
+    async def on_game_input_submitted(self, event: Input.Submitted):
+        output = self.query_one("#output", Static)
         user_game = event.value
+        appid = self.find_appid(self.apps, user_game)
+
+        if appid is None:
+            output.update('There is no such game.')
+            return
+
+        try:
+            url = f"https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid={appid}"
+            response = await asyncio.to_thread(requests.get, url)
+            data = response.json()
+            player_count = data['response'].get('player_count')
+            if appid and player_count is not None:
+                output.update(f'{user_game} — {player_count} players online!')
+        except Exception as e:
+            output.update(f'Unexpected error: {e}')
 
 MyApp().run()
